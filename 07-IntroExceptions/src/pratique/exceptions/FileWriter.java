2998d7bed09a782a56cbc6fb710a59198628aae8
valdoth <valdotsiarohasina@gmail.com>
2022-05-25 21:14:56 +0300

    try finally et try with ressurces

--- 07-IntroExceptions/src/pratique/exceptions/FileWriter.java
+++ 07-IntroExceptions/src/pratique/exceptions/FileWriter.java
@@ -1,7 +1,59 @@
 package pratique.exceptions;
 
-public class FileWriter {
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+class Demo implements AutoCloseable {
 	
+	public Demo() {
+		System.out.println("Ressource instanciée");
+	}
+	
+	@Override
+	public void close() throws Exception {
+		System.out.println("Ressource libérée");
+	}
+}
+
+public class FileWriter {
+	
+	public static void saveDateTryWithRessources() {
+		try (Demo d = new Demo()) {
+			
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+
+	public static void saveDataFinally() {
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream("save.data");
+			fos.write("Ok\n".getBytes());			
+			System.out.println("Écriture dans le fichier terminée");
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		} finally {
+			//System.out.println("In the finally bloc");
+			if (fos != null) {
+				try {
+					fos.close();
+					System.out.println("fermeture du fichier");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		System.out.println("Bye bye");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		saveDataFinally();
+		saveDateTryWithRessources();
+		
+	}
 	
 }
